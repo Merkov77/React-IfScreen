df20c4830f67f0d6db0a9572c8f80fa00dbf95f8
José Mavo <34657109+Merkov77@users.noreply.github.com>
2020-04-17 23:10:07 -0400

    Update ifscreen.js

--- src/ifscreen.js
+++ src/ifscreen.js
@@ -37,12 +37,5 @@ class IfScreen extends React.Component{
 	}
 }
 
-let config = {
-	case1 : "scwidth >= 1000", //scwidth init  >= 1000px
-	case2 : "scwidth >= 801 && scwidth <= 999", //scwidth init  >= 801px && <= 999px
-	case3 : "scwidth >= 650 && scwidth <= 800", //scwidth init  >= 650px && <= 800px
-	case4 : "scwidth <= 649" //scwidth init  <= 649px
-}
+export default IfScreen;
 
-
-//When your use the IfScreen component, you don't have define "()" in functions only if the functions to execute are not a component JSX. Example: logic functions. Yet, if you want to define a JSX component, you have to write "()"
